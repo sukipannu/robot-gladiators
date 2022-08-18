7c70f08a66363e62d1a9d8c7429ad92b670295e8
Your Name <you@example.com>
2022-08-17 20:42:25 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -29,8 +29,8 @@ var fight = function(enemyName) {
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(
+    enemyHealth = Math.max(0, enemyHealth - playerAttack);
+        console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
 
@@ -48,8 +48,8 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
-    console.log(
+    playerHealth = Math.max(0, playerHealth - enemyAttack);
+        console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
 
@@ -82,7 +82,8 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
+
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
@@ -179,6 +180,13 @@ var shop = function() {
       break;
   }
 };
+
+// function to generate a random numeric value
+var randomNumber = function(40, 60) {
+    var value = Math.floor(Math.random() * (60 - 40 + 1) + 40);
+  
+    return value;
+  };
 
 // start first game when page loads
 startGame();
